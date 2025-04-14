11e7ea2c5684c56cf13a01754bb4c8aae75f141a
Jack <jackfehan@gmail.com>
2025-04-14 14:42:02 -0400

    Updated for PreOrder Method

--- BST.h
+++ BST.h
@@ -197,3 +197,11 @@ printhelp(BSTNode<Key, E>* root, int level) const {
   visit(root);						  // Print node value
   printhelp(root->right(), level+1);  // Do right subtree
 }
+
+void BST<Key, E>::
+printPreOrder(BSTNode<Key, E>* root, int level) const {
+  if (root == NULL) return;           // Empty tree
+  visit(root);						  // Print node value
+  printPreOrder(root->left(), level+1);   // Do left subtree
+  printPreOrder(root->right(), level+1);  // Do right subtree
+}
